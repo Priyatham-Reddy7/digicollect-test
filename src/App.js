b5896eb39b9f25edfd5b414e0f6f66263b42ff35
Priyatham Reddy <priyathamreddy147@gmail.com>
2021-12-10 16:16:34 +0530

    Data fetch from API done..

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import ProfileCard from './Components/ProfileCard';
+
 
 function App() {
+  const [users, setUsers] = useState([]);
+  
+  //Fetching data from API
+  const getResponse = async () => {
+    const response = await fetch('https://reqres.in/api/users')
+    const output = await response.json();
+    setUsers(output.data)
+    console.log(users);
+  }
+  //Running the API function as soon as the page gets loaded
+  useEffect(getResponse, [])
+
   return (
     <div className="App">
-        <h1>Hello there....just a test</h1>
+        {users.map((user)=>(
+          <ProfileCard 
+
+          />
+          ))}
     </div>
   );
 }
