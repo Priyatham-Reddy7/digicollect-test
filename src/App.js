aca47d3580d3d6f25e9ede0cbdbe71a858cd206e
Priyatham Reddy <priyathamreddy147@gmail.com>
2021-12-10 16:46:10 +0530

    Created a separate profile component

--- src/App.js
+++ src/App.js
@@ -7,20 +7,23 @@ function App() {
   const [users, setUsers] = useState([]);
   
   //Fetching data from API
-  const getResponse = async () => {
+  useEffect(() =>{
+    const getResponse = async () =>{
     const response = await fetch('https://reqres.in/api/users')
     const output = await response.json();
-    setUsers(output.data)
-    console.log(users);
-  }
-  //Running the API function as soon as the page gets loaded
-  useEffect(getResponse, [])
+    setUsers(output.data)}
+    getResponse();
+  }, [])
 
   return (
     <div className="App">
         {users.map((user)=>(
           <ProfileCard 
-
+              key={user.id}
+             fname={user.first_name}
+             lname={user.last_name}
+             imageURL={user.avatar}
+             email={user.email}
           />
           ))}
     </div>
--- src/Components/ProfileCard.js
+++ src/Components/ProfileCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const ProfileCard = () => {
+const ProfileCard = ({fname, lname, imageURL, email}) => {
     return (
         <div>
-            
+            <img src={imageURL} alt="profile_img" />
+            <h3>{fname} {lname}</h3>
+            <h5>Email: {email}</h5>
         </div>
     )
 }
